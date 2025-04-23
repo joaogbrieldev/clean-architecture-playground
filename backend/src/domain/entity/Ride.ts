1f56651e6d0b36431bd440ca3f0ef0ec289c4d35
João Figueiredo <joao.ggwp@outlook.com>
2025-04-22 23:15:35 -0300

    fix: ajust ride aggregate

--- backend/src/domain/entity/Ride.ts
+++ backend/src/domain/entity/Ride.ts
@@ -1,24 +1,31 @@
 import crypto from "crypto";
 import Coord from "../vo/Coord";
+import UUID from "../vo/UUID";
 
 export default class Ride {
   private from: Coord;
   private to: Coord;
+  private rideId: UUID;
+  private passengerId: UUID;
+  private driverId?: UUID;
   constructor(
-    readonly rideId: string,
-    readonly passengerId: string,
-    readonly driverId: string | null,
+    rideId: string,
+    passengerId: string,
+    driverId: string | null,
     fromLat: number,
     fromLong: number,
     toLat: number,
     toLong: number,
     readonly fare: number,
     readonly distance: number,
-    readonly status: string,
+    private status: string,
     readonly date: Date
   ) {
     this.from = new Coord(fromLat, fromLong);
     this.to = new Coord(toLat, toLong);
+    this.rideId = new UUID(rideId);
+    this.passengerId = new UUID(passengerId);
+    if (driverId) this.driverId = new UUID(driverId);
   }
 
   static create(
@@ -47,6 +54,34 @@ export default class Ride {
       date
     );
   }
+
+  accept(driverId: string) {
+    this.driverId = new UUID(driverId);
+    if (this.status !== "requested") throw new Error("Invalid status");
+    this.status = "accepted";
+  }
+
+  start() {
+    if (this.status !== "accepted") throw new Error("Invalid status");
+    this.status = "in_progress";
+  }
+
+  getStatus() {
+    return this.status;
+  }
+
+  getRideId() {
+    return this.rideId.getValue();
+  }
+
+  getPassengerId() {
+    return this.passengerId.getValue();
+  }
+
+  getDriverId() {
+    return this.driverId?.getValue();
+  }
+
   getFrom() {
     return this.from;
   }
--- backend/src/infra/repository/RideRepository.ts
+++ backend/src/infra/repository/RideRepository.ts
@@ -1,13 +1,14 @@
-// Repository
-
 import Ride from "../../domain/entity/Ride";
 import DatabaseConnection from "../database/DatabaseConnection";
+
+// Repository
 
 export default interface RideRepository {
   saveRide(ride: Ride): Promise<void>;
   getRideById(rideId: string): Promise<Ride>;
   hasActiveRideByPassengerId(passengerId: string): Promise<boolean>;
   hasActiveRideByDriverId(passengerId: string): Promise<boolean>;
+  updateRide(ride: Ride): Promise<void>;
 }
 
 export class RideRepositoryDatabase implements RideRepository {
@@ -37,16 +38,16 @@ export class RideRepositoryDatabase implements RideRepository {
     await this.connection.query(
       "insert into ccca.ride (ride_id, passenger_id, driver_id, from_lat, from_long, to_lat, to_long, fare, distance, status, date) values ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)",
       [
-        ride.rideId,
-        ride.passengerId,
-        ride.driverId,
+        ride.getRideId(),
+        ride.getPassengerId(),
+        ride.getDriverId(),
         ride.getFrom().getLat(),
         ride.getFrom().getLong(),
         ride.getTo().getLat(),
         ride.getTo().getLong(),
         ride.fare,
         ride.distance,
-        ride.status,
+        ride.getStatus(),
         ride.date,
       ]
     );
@@ -59,11 +60,19 @@ export class RideRepositoryDatabase implements RideRepository {
     );
     return !!rideData;
   }
+
   async hasActiveRideByDriverId(driverId: string) {
     const [rideData] = await this.connection.query(
-      "select 1 from ccca.ride where driver_id = $1 and status not in ('accepted', 'in_progress') limit 1",
+      "select 1 from ccca.ride where driver_id = $1 and status not in ('completed', 'cancelled') limit 1",
       [driverId]
     );
     return !!rideData;
+  }
+
+  async updateRide(ride: Ride) {
+    await this.connection.query(
+      "update ccca.ride set status = $1, driver_id = $2 where ride_id = $3",
+      [ride.getStatus(), ride.getDriverId(), ride.getRideId()]
+    );
   }
 }
